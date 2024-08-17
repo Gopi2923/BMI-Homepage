b49f3294816f2b853878252f909d46eb3f7278fb
Gopi2923 <dgopidhikshan@gmail.com>
2024-08-17 13:15:32 +0530

    Added packages.

---
+++ public/electron.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // Ensure this file exists or remove this line if not needed
+    },
+  });
+
+  win.loadURL('http://localhost:3000'); // Ensure this URL is correct
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
