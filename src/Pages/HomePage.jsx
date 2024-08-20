63b7b9548fb8a19179cea5f203c0ebeab8037e9a
Gopi2923 <dgopidhikshan@gmail.com>
2024-08-20 10:38:56 +0530

    integrated apis.

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,23 +1,90 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import QRCode from 'qrcode.react';
 import './HomePage.css';
 import logo from '../Assets/logo.png';
-import healthImage from '../Assets/backgroundimg01.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleRight } from '@fortawesome/free-solid-svg-icons';
-
+import { faCircleRight, faCircleXmark, faCircleCheck } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 const HomePage = () => {
   const [showPaymentModal, setShowPaymentModal] = useState(false);
-  const [paymentConfirmed, setPaymentConfirmed] = useState(false);
+  const [upiLink, setUpiLink] = useState('');
+  const [transactionId, setTransactionId] = useState('');
+  const [paymentStatus, setPaymentStatus] = useState('pending'); // 'pending', 'success', 'failure'
 
-  const handleInstantReportClick = () => {
-    setShowPaymentModal(true);
+  useEffect(() => {
+    let interval;
+    let timeout;
+    if (transactionId) {
+      interval = setInterval(() => checkPaymentSuccess(transactionId), 3000);
+      timeout = setTimeout(() => {
+        clearInterval(interval);
+        setPaymentStatus('failure');
+      }, 180000); // 3 minutes timeout
+    }
+
+    return () => {
+      clearInterval(interval);
+      clearTimeout(timeout);
+    };
+  }, [transactionId]);
+
+  const generateOrderId = () => {
+    return Math.floor(1000000000 + Math.random() * 9000000000).toString();
   };
 
-  const handlePaymentConfirmation = () => {
-    setPaymentConfirmed(true);
-    redirectToAndroidApp();
+  const checkPaymentSuccess = async (transactionId) => {
+    try {
+      const response = await axios.get(`https://ssrvd.onrender.com/payment-gateway/paymentStatus/${transactionId}`);
+      if (response.data.data === true) {
+        setPaymentStatus('success');
+      }
+    } catch (error) {
+      console.error('Error checking payment status:', error);
+      setPaymentStatus('failure');
+    }
+  };
+
+  const handleInstantReportClick = async (e) => {
+    setShowPaymentModal(true);
+    e.preventDefault();
+    const orderId = generateOrderId();
+    try {
+      const token = '367|qM5tv66Rhk8Tm13DlvDkc92KNwVMvAhOuljLB8tA';
+      const transactionData = {
+        amount: '1',
+        description: 'Health ATM Report',
+        name: 'Gopi',
+        email: 'dhanushnm07@gmail.com',
+        mobile: Number('1234567890'),
+        enabledModesOfPayment: 'upi',
+        payment_method: 'UPI_INTENT',
+        source: 'api',
+        order_id: orderId,
+        user_uuid: 'swp_sm_903dd099-3a9e-4243-ac1e-f83f83c30725',
+        other_info: 'api',
+        encrypt_response: 0
+      };
+
+      const formData2 = new FormData();
+      for (const key in transactionData) {
+        formData2.append(key, transactionData[key]);
+      }
+
+      const transactionResponse = await axios.post('https://www.switchpay.in/api/createTransaction', formData2, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+
+      const { upi_intent_link, transaction_id } = transactionResponse.data;
+      setUpiLink(upi_intent_link);
+      setTransactionId(transaction_id);
+
+    } catch (error) {
+      console.error('Error submitting form:', error.response ? error.response.data : error.message);
+    }
   };
 
   const redirectToAndroidApp = () => {
@@ -28,10 +95,20 @@ const HomePage = () => {
     setShowPaymentModal(false);
   };
 
+  useEffect(() => {
+    if (paymentStatus === 'success') {
+      setTimeout(() => {
+        redirectToAndroidApp();
+      }, 2000); // Redirect after showing success modal for 2 seconds
+    }
+  }, [paymentStatus]);
+
   return (
     <div className="homepage-container">
       {/* Logo */}
-      <img src={logo} alt="Atmaprikash Logo" className="logo" />
+      <div className="image-container">
+        <img src={logo} alt="Atmaprikash Logo" className="logo" />
+      </div>
 
       {/* Title */}
       <h1 className="title">Health ATM (Vitals Checking Machine)</h1>
@@ -41,53 +118,63 @@ const HomePage = () => {
 
       <div className="instant-report-section">
         <button className="instant-report-button" onClick={handleInstantReportClick}>
-          Check Your Vitals <FontAwesomeIcon icon={faCircleRight} shake size="2xl" style={{color: "#FFD43B",}} />
+          Check Your Vitals <FontAwesomeIcon icon={faCircleRight} shake size="2xl" style={{ color: "#FFD43B" }} />
         </button>
       </div>
 
       {showPaymentModal && (
         <div className="modal-overlay">
           <div className="modal-content">
-            <h2>Pay 99 INR to Proceed</h2>
-            <QRCode value="http://example.com/payment" size={328} />
-            <button className="confirm-payment-button" onClick={handlePaymentConfirmation}>
-              Confirm Payment
-            </button>
-            <button className="close-modal-button" onClick={closeModal}>
-              Close
-            </button>
+            {paymentStatus === 'pending' && (
+              <>
+                <h2>Pay 99 INR to Proceed</h2>
+                <QRCode value={upiLink} size={328} />
+                <div className="button-container">
+                  <button className="close-modal-button" onClick={closeModal}>
+                    Close
+                  </button>
+                </div>
+              </>
+            )}
+            {paymentStatus === 'success' && (
+              <div className="success-modal">
+                <h2>Payment Successful!</h2>
+                <FontAwesomeIcon icon={faCircleCheck} size="2xl" style={{color: "#0ff05e",}} />
+              </div>
+            )}
+            {paymentStatus === 'failure' && (
+              <div className="failure-modal">
+                <h2>Payment Failed</h2>
+                <FontAwesomeIcon icon={faCircleXmark} size="6x" style={{color: "#e00b2b",}} />
+                <button className="close-modal-button" onClick={closeModal}>
+                  Close
+                </button>
+              </div>
+            )}
           </div>
-        </div>
-      )}
-
-      {paymentConfirmed && (
-        <div className="redirecting-section">
-          {/* <p>Redirecting to the CoWin BMI app...</p> */}
         </div>
       )}
 
       {/* Image and Parameters List */}
       <div className="image-parameters-section">
-        {/* <img src={healthImage} alt="Health Check" className="health-image" /> */}
-
         <div className="parameters-section">
           <ul className="parameters-list left-list">
-            <li><i className="fas fa-user"></i> Age</li>
-            <li><i className="fas fa-ruler-vertical"></i> Height</li>
-            <li><i className="fas fa-weight"></i> Weight</li>
-            <li><i className="fas fa-calculator"></i> Body Mass Index</li>
-            <li><i className="fas fa-apple-alt"></i> Nutritional Status</li>
-            <li><i className="fas fa-weight-hanging"></i> Ideal Body Weight</li>
-            <li><i className="fas fa-percentage"></i> Body Fat</li>
+            <li><i className="fas fa-user"></i> Age / ವಯಸ್ಸು</li>
+            <li><i className="fas fa-ruler-vertical"></i> Height / ಎತ್ತರ</li>
+            <li><i className="fas fa-weight"></i> Weight / ತೂಕ</li>
+            <li><i className="fas fa-calculator"></i> Body Mass Index / ದೇಹದ ಮಾಪಕ</li>
+            <li><i className="fas fa-apple-alt"></i> Nutritional Status / ಪೋಷಕ ಸ್ಥಿತಿ</li>
+            <li><i className="fas fa-weight-hanging"></i> Ideal Body Weight / ಆದರ್ಶ ದೇಹದ ತೂಕ</li>
+            <li><i className="fas fa-percentage"></i> Body Fat / ದೇಹದ ಕೊಬ್ಬು</li>
           </ul>
           <ul className="parameters-list right-list">
-            <li><i className="fas fa-water"></i> Total Body Water</li>
-            <li><i className="fas fa-burn"></i> Basal Metabolic Rate</li>
-            <li><i className="fas fa-weight"></i> Fat Mass</li>
-            <li><i className="fas fa-dumbbell"></i> Lean/Skeletal Body Mass</li>
-            <li><i className="fas fa-exclamation-circle"></i> Overweight By</li>
-            <li><i className="fas fa-clipboard-check"></i> Recommendations</li>
-            <li><i className="fas fa-star"></i> Your Lucky Message</li>
+            <li><i className="fas fa-water"></i> Total Body Water / ಒಟ್ಟು ದೇಹದ ನೀರು</li>
+            <li><i className="fas fa-burn"></i> Basal Metabolic Rate / ಮೂಲವ್ಯೂಪಚಯ ದರ</li>
+            <li><i className="fas fa-weight"></i> Fat Mass / ಕೊಬ್ಬಿನ ಪ್ರಮಾಣ</li>
+            <li><i className="fas fa-dumbbell"></i> Lean/Skeletal Body Mass / ಕುಳಿತ ದೇಹದ ಪ್ರಮಾಣ</li>
+            <li><i className="fas fa-exclamation-circle"></i> Overweight By / ಅಧಿಕ ತೂಕದ ಮೂಲಕ</li>
+            <li><i className="fas fa-clipboard-check"></i> Recommendations / ಶಿಫಾರಸುಗಳು</li>
+            <li><i className="fas fa-star"></i> Your Lucky Message / ನಿಮ್ಮ ಭಾಗ್ಯದ ಸಂದೇಶ</li>
           </ul>
         </div>
       </div>
